bbd5c1f81fe0b25a9b4894b99e7d7e02d286cf95
memory-overflow <willzhen@tencent.com>
2025-04-25 19:30:54 +0800

    mcp 工具信息增加缓存

--- tool/mcp.go
+++ tool/mcp.go
@@ -10,66 +10,110 @@ import (
 	"github.com/mark3labs/mcp-go/mcp"
 )
 
-// McpTool ...
-type McpTool struct {
-	Name        string
-	Description string
-	Schame      map[string]interface{}
-	Cli         client.MCPClient
+type mcpClientCache struct {
+	Cli           client.MCPClient
+	Data          map[string]mcp.Tool
+	LastFetchTime time.Time
 }
 
-// GetName returns the name of the tool
-func (m *McpTool) GetName() string {
-	m.fetch()
-	return m.Name
-}
-
-// GetDescription returns the description of the tool
-func (m *McpTool) GetDescription() string {
-	m.fetch()
-	return m.Description
-}
-
-// GetParametersSchema returns the JSON schema for the tool parameters
-func (m *McpTool) GetParametersSchema() map[string]interface{} {
-	m.fetch()
-	return m.Schame
-}
-
-// Execute executes the tool with the given parameter
-func (m *McpTool) Execute(ctx context.Context, params map[string]interface{}) (interface{}, error) {
-	req := mcp.CallToolRequest{}
-	req.Params.Name = m.Name
-	req.Params.Arguments = params
-	result, err := m.Cli.CallTool(ctx, req)
-	if err != nil {
-		return nil, err
+func (cache *mcpClientCache) GetDescription(name string) string {
+	cache.fetch()
+	if info, ok := cache.Data[name]; ok {
+		return info.Description
 	}
-	return result, nil
+	return ""
 }
 
-func (m *McpTool) fetch() {
+func (cache *mcpClientCache) GetParametersSchema(name string) map[string]interface{} {
+	cache.fetch()
+	schema := map[string]interface{}{}
+	if info, ok := cache.Data[name]; ok {
+		bs, _ := json.Marshal(info.InputSchema)
+		_ = json.Unmarshal(bs, &schema)
+		return schema
+	}
+	return schema
+}
+
+// 构建一个新的 mcp client cache
+func NewMcpClientCache(cli client.MCPClient) (*mcpClientCache, error) {
+	if cli == nil {
+		return nil, fmt.Errorf("mcp client is nil")
+	}
+	rsp, err := ListMcpTools(cli)
+	if err != nil {
+		return nil, fmt.Errorf("mcp client is list tools error: %v", err)
+	}
+	cache := &mcpClientCache{
+		Cli:           cli,
+		LastFetchTime: time.Now(),
+		Data:          map[string]mcp.Tool{},
+	}
+	for _, tool := range rsp.Tools {
+		cache.Data[tool.Name] = tool
+	}
+	return cache, nil
+}
+
+func (cache *mcpClientCache) fetch() {
 	defer func() {
 		if p := recover(); p != nil {
 			return
 		}
 	}()
-
-	rsp, err := ListMcpTools(m.Cli)
-	if err != nil {
-		// 如果失败，继续用缓存数据
-		return
-	}
-	for _, tool := range rsp.Tools {
-		if tool.Name == m.Name {
-			m.Description = tool.Description
-			bs, _ := json.Marshal(tool.InputSchema)
-			tmpSchema := map[string]interface{}{}
-			if err := json.Unmarshal(bs, &tmpSchema); err != nil {
-				m.Schame = tmpSchema
-			}
+	// 2s 刷新一次
+	if time.Now().After(cache.LastFetchTime.Add(2 * time.Second)) {
+		rsp, err := ListMcpTools(cache.Cli)
+		if err != nil {
+			return
+		}
+		cache.LastFetchTime = time.Now()
+		for _, tool := range rsp.Tools {
+			cache.Data[tool.Name] = tool
 		}
 	}
+}
+
+// McpTool ...
+type McpTool struct {
+	Name  string
+	Cache *mcpClientCache
+}
+
+// GetName returns the name of the tool
+func (m *McpTool) GetName() string {
+	return m.Name
+}
+
+// GetDescription returns the description of the tool
+func (m *McpTool) GetDescription() string {
+	if m.Cache != nil {
+		return m.Cache.GetDescription(m.GetName())
+	}
+	return ""
+}
+
+// GetParametersSchema returns the JSON schema for the tool parameters
+func (m *McpTool) GetParametersSchema() map[string]interface{} {
+	if m.Cache != nil {
+		return m.Cache.GetParametersSchema(m.GetName())
+	}
+	return map[string]interface{}{}
+}
+
+// Execute executes the tool with the given parameter
+func (m *McpTool) Execute(ctx context.Context, params map[string]interface{}) (interface{}, error) {
+	if m.Cache == nil || m.Cache.Cli == nil {
+		return nil, fmt.Errorf("mcp client is nil")
+	}
+	req := mcp.CallToolRequest{}
+	req.Params.Name = m.Name
+	req.Params.Arguments = params
+	result, err := m.Cache.Cli.CallTool(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 // ListMcpTools 获取 mcp 工具列表
